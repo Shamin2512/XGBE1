e0a93d089da2fd6f49b852c4612061b169fef434
Shamin2512 <shamin.tahasildar@gmail.com>
2022-10-11 17:07:14 +0100

    XGB training with RMSE evaluation metric

--- Example1XGB.py
+++ Example1XGB.py
@@ -26,13 +26,28 @@ df.replace(' ', '_', regex=True, inplace=True) #Replace all blank spaces with un
 X = df.drop('dataset', axis=1).copy() #X is dataframe with data used to train and predict if SNP or PD 
 y_encoded = pd.get_dummies(df, columns=['dataset'], prefix=['Mutation']) #y is dataframe with mutations in non-object format for testing
 y = y_encoded[['Mutation_pd','Mutation_snp']].copy()
+print(y)
+print(X)
 
 #**Split data into training and test**
+
 #y_count = y.drop(['Binding', 'SProtFT0', 'SProtFT1', 'SProtFT2', 'SProtFT3', 'SProtFT4', 'SProtFT5', 'SProtFT6', 'SProtFT7', 'SProtFT8', 'SProtFT9', 'SProtFT10', 'SProtFT11', 'SProtFT12', 'Interface', 'Relaccess', 'Impact', 'HBonds', 'SPhobic', 'CPhilic', 'BCharge', 'SSGeom', 'Voids', 'MLargest1', 'MLargest2', 'MLargest3', 'MLargest4', 'MLargest5', 'MLargest6', 'MLargest7', 'MLargest8', 'MLargest9', 'MLargest10', 'NLargest1', 'NLargest2', 'NLargest3', 'NLargest4', 'NLargest5', 'NLargest6', 'NLargest7', 'NLargest8', 'NLargest9', 'NLargest10', 'Clash', 'Glycine', 'Proline', 'CisPro'],axis=1)
 #print(y_count.count()) #Shows data is already balanced
-#X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y) #Splits data into training and testing (even tho already split)
 
-print(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y) #Splits data into training and testing (even tho already split)
+
+#**Build XGB training/ classification model**
+clf_xgb = xgb.XGBClassifier(objective='binary:logistic', seed=42)
+clf_xgb.fit(
+    X_train,
+    y_train,
+    early_stopping_rounds =10,
+    verbose=True,
+    eval_metric='rmse',
+    eval_set=[(X_test, y_test)])
+#Stop building trees 10 rounds after prediction does not improve. Area Under Curve evalutation. Test set used to decide how many trees to build
+print(clf_xgb)
+
 
 
 
