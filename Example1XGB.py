a0205b2a1cca7f694dfb465e9c1915f3b88dd484
Shamin2512 <shamin.tahasildar@gmail.com>
2022-10-12 15:29:16 +0100

    Using new Dmatrix method

--- Example1XGB.py
+++ Example1XGB.py
@@ -15,6 +15,7 @@ from sklearn.model_selection import GridSearchCV #Cross validation to improve hy
 from sklearn.metrics import confusion_matrix #creates the confusion matrix - stats on how accurate the test set output is
 from sklearn.metrics import ConfusionMatrixDisplay #draws the confusion matrix
 
+
 #**Create, clean and convert the train.csv dataset to a dataframe**
 pd.set_option('display.max_columns', None) #Max columns
 df = pd.read_csv('demo.csv') #Pandas creates data frame from the .csv mutation data
@@ -22,18 +23,20 @@ df.drop(['pdbcode:chain:resnum:mutation'],axis=1, inplace=True) #removes columns
 df.columns = df.columns.str.replace(' ', '_') #Removes gaps in column names
 df.replace(' ', '_', regex=True, inplace=True) #Replace all blank spaces with underscore (none were present)
 
+
 #**One-Hot Encoding the categorical data for dataframe y**
 X = df.drop('dataset', axis=1).copy() #Dataframe with data used to train and predict if SNP or PD
-#X = X_drop.head(50000) #Takes first 50000 rows as datapoints for training since data is random order
-#X.to_csv('xtrain.csv') #Output .csv
-
-#y_tail = df.tail(11205)
 y_encoded = pd.get_dummies(df, columns=['dataset'], prefix=['Mutation']) #Dataframe with mutations in numerical categorical format for testing
 y = y_encoded[['Mutation_pd','Mutation_snp']].copy() #y is dataframe with only mutation columns for testing against
-#y.to_csv('ytest.csv')
+
+
+#**Converting the pandas dataframe to a XGB Dmatrix**
+data_dmatrix = xgb.Dmatrix(data= X, label =y)
+
 
 #**Split data into training and test**
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.81694, random_state=42, stratify=y) #Splits data into training and testing, specifying the training set should be 50,000 rows
+
 
 #**Build XGB training/ classification model**
 clf = xgb.XGBClassifier(objective='binary:logistic', seed=42)
