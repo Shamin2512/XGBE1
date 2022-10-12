a0e20d6960eef0a1599882aef1db90a05ed304e6
Shamin2512 <shamin.tahasildar@gmail.com>
2022-10-12 18:16:16 +0100

    y df is no longer multiclass; only contains 1 column for PD

--- Example1XGB.py
+++ Example1XGB.py
@@ -16,25 +16,18 @@ from sklearn.metrics import confusion_matrix #creates the confusion matrix - sta
 from sklearn.metrics import ConfusionMatrixDisplay #draws the confusion matrix
 
 #**Create, clean and convert the train.csv dataset to a dataframe**
-pd.set_option('display.max_columns', None) #Max columns
 df = pd.read_csv('demo.csv') #Pandas creates data frame from the .csv mutation data
-df.drop(['pdbcode:chain:resnum:mutation'],axis=1, inplace=True) #removes columns undeeded in training
+df.drop(['pdbcode:chain:resnum:mutation'],axis=1, inplace=True) #removes columns unrequired columns, replacing the variable 
 df.columns = df.columns.str.replace(' ', '_') #Removes gaps in column names
 df.replace(' ', '_', regex=True, inplace=True) #Replace all blank spaces with underscore (none were present)
 
 #**Encoding the categorical data for dataframe y**
 X = df.drop('dataset', axis=1).copy() #X is dataframe with data used to train and predict if SNP or PD 
-y_encoded = pd.get_dummies(df, columns=['dataset'], prefix=['Mutation']) #y is dataframe with mutations in non-object format for testing
-y = y_encoded[['Mutation_pd','Mutation_snp']].copy()
-print(y)
-print(X)
+y_encoded = pd.get_dummies(df, columns=['dataset']) #y is df with mutations changing from object -> unint8 (integer)
+y = y_encoded['dataset_pd'].copy()
 
 #**Split data into training and test**
-
-#y_count = y.drop(['Binding', 'SProtFT0', 'SProtFT1', 'SProtFT2', 'SProtFT3', 'SProtFT4', 'SProtFT5', 'SProtFT6', 'SProtFT7', 'SProtFT8', 'SProtFT9', 'SProtFT10', 'SProtFT11', 'SProtFT12', 'Interface', 'Relaccess', 'Impact', 'HBonds', 'SPhobic', 'CPhilic', 'BCharge', 'SSGeom', 'Voids', 'MLargest1', 'MLargest2', 'MLargest3', 'MLargest4', 'MLargest5', 'MLargest6', 'MLargest7', 'MLargest8', 'MLargest9', 'MLargest10', 'NLargest1', 'NLargest2', 'NLargest3', 'NLargest4', 'NLargest5', 'NLargest6', 'NLargest7', 'NLargest8', 'NLargest9', 'NLargest10', 'Clash', 'Glycine', 'Proline', 'CisPro'],axis=1)
-#print(y_count.count()) #Shows data is already balanced
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y) #Splits data into training and testing (even tho already split)
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.81694, random_state=42, stratify=y) #Splits data into training (81.694%) and testing (18.216%). 
 
 #**Build XGB training/ classification model**
 clf = xgb.XGBClassifier(objective='binary:logistic', seed=42)
@@ -43,7 +36,7 @@ clf.fit(
     y_train,
     early_stopping_rounds=10,
     verbose=True,
-    eval_metric='rmse',
+    eval_metric='aucpr',
     eval_set=[(X_test, y_test)]) #Rmse metric, early stopping rounds incompatible with fit()
 print(clf)
 
